00e2b904f9518e2bc4830b4803d3904ce450319b
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-06-14 18:09:06 -0400

    project complete

--- server.js
+++ server.js
@@ -13,10 +13,48 @@ const app = express();
 app.use(morgan('common'));
 app.use("/blogposts", blogPostsRouter);
 
-//adding blogPosts for use with app+
+//both runServer and closeServer need to access the same
+//server object. Therefore we declare 'server' here and then
+//when runServer runs, it assigns a value.
 
+let server;
 
+//this function starts the server and returns a Promise.
+//In the test code, we need a way of asynchronously starting
+//the server given the promises will be there.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// like 'runServer', this function also needs to return an promise.
+//'server.close' does not return a promise on its own, so we manually
+//create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't call 'resolve()'
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+//if server.js is called directly ('as with 'node server.js'), this block
+//runs. But we also export the runServer command so other code ( for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
